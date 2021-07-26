3521dbb5b19a1d4df54c7e1ff94c3664b0889b2e
Matthew Costabile <matt@mattc.io>
2021-07-26 14:32:15 +0000

    add gitconfig settings

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+cat .gitconfig >> "$HOME/.gitconfig"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
