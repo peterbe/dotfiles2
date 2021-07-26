20b2ab5906f888973e8df141390ed0dfe43cf4d6
Matthew Costabile <matt@mattc.io>
2021-07-26 14:36:30 +0000

    fix pwd

--- install.sh
+++ install.sh
@@ -17,9 +17,9 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-cat .gitconfig >> "$HOME/.gitconfig"
+cat $(pwd)/.gitconfig >> "$HOME/.gitconfig"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-cp -f .zshrc $HOME
+cp -f $(pwd)/.zshrc $HOME
 source "$HOME/.zshrc"
